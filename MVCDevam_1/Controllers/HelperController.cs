02a9b69067fdf0ce98c0ae1c0cce78cae3427dee
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-04-11 14:28:25 +0300

    Add project files.

---
+++ MVCDevam_1/Controllers/HelperController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDevam_1.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Index()
+        {
+             return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDevam_1/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using MVCDevam_1.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDevam_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            #region OnemliNotlar
+
+            //View metodunun farklı bir View döndürmesi icerisine explicit(acıkca) string bir tipte argüman verilmesiyle olur... BU metodun bir de object tipinde bir parametresi vardır...Bu object tipindeki parametreli overload'inin görevi de View'a Model göndermektir...Bir View metodunun farklı bir View döndürmesi ile Model göndermesi arasında fark vardır...Bu metotta acık bir şekilde bir string parametre tanımlanmıs ve aynı zamanda bir object tipinde parametre de tanımlanarak overload yapılmıstır... object tipinde parametreli overload'i icerisine argüman olarak verilen her tipi kabul etse de bu metoda acık acık string tipinde bir argüman verilirse,acık acık string tipinde tanımlanmıs bir parametresi oldugu icin string parametreli overload'ı calısır(yani size o isimde bir View döndürmeye calısır)...Dolayısıyla siz bir string tipinde model göndermiş degil farklı bir View döndürmüş olursunuz...
+
+
+            //Model göndermek Front End'de göstermek istedigimiz bilgileri Back End'den Front End'e yollamanın bir yoludur...Eger bu bilgi string tipte yollanacak ise o zaman string verisinin object'e cast edilerek argüman olarak verilmesi gerekir("Deneme" as object)
+
+            //Model göndermek cok performanslı ve oldukca yetenekli bir yöntem oldugundan dolayı kompleks modelleri destekleyen en iyi yöntemdir. Digerleri de destekler ama bunu yaparken performans düsürürler...
+
+
+            #endregion
+
+            List<Product> urunler = new List<Product>
+            {
+                new Product
+                {
+                    ProductName = "Tadelle",
+                    ID = 1,
+                    UnitPrice = 8
+                },
+                new Product
+                {
+                    ProductName = "Biskrem",
+                    ID = 2,
+                    UnitPrice  = 5
+                },
+                new Product
+                {
+                    ProductName = "Cizi",
+                    ID = 3,
+                    UnitPrice = 3
+                }
+            };
+           
+            //Biz burada bir View'a model gönderiyoruz...
+
+            //Bir View'a model gönderiyorsanız kesinlikle View'in bu modeli karsılaması gerekir...
+
+            //Bİr View, gelen modeli kücük m ile yazılan @model keyword'u ile karsılar...Bir View, modeli karsılarken modelin hangi tipte hangi tipte geldigini bilmek zorundadır...Baska bir şeye gerek yoktur...
+
+
+            //Her bir View bir Model property'sine sahiptir...Eger bir View Model karsılamak istiyorsa bu ilgili property'sinin set ozelleştirilmiş metoduna gider...Yani View'in Model property'sinin set metodu calısmalıdır ki Model karsılansın...Bir View icerisinde bu set'i @model keyword'u ile RazorView sayesinde calıstırırsınız...BUradaki keyword'de kücük m'ye dikkat edilmelidir...Cünkü get yapmak icin de @Model keyword'unu calıstırmalısınız...(büyük M)
+
+
+            //Bir View'in sadece bir modeli olabilir...
+
+
+
+            return View(urunler);
+        }
+    }
+}
